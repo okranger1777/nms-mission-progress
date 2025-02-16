78a2d37aeb0d90c02cb148cf8eeb2b219dac3327
Ok Ranger <159083282+okranger1777@users.noreply.github.com>
2025-02-16 17:05:28 +0000

    Fix line numbers

--- scripts/hotkeys.py
+++ scripts/hotkeys.py
@@ -2,13 +2,11 @@
 import io
 import sys
 import json
-
 if len(sys.argv) < 2:
   print(f'Usage: {sys.argv[0]} EXPORTED-NMS-SAVE-JSON-FILE')
   sys.exit()
 with io.open(sys.argv[1],'r',encoding="utf-8") as myfile: mystring = myfile.read()
-data = json.loads(mystring)
-# line numbers: 10-19 for Player Hotkeys 0-9, lines 20-29 for Ship, 30-39 for Exocraft
+data = json.loads(mystring) # line numbers: 10-19 for Player Hotkeys 0-9, lines 20-29 for Ship, 30-39 for Exocraft
 player = [ { "Action": {"QuickMenuActions": "None"}, "Id": "^", "Number": 0, "InventoryIndex": {"X": -1, "Y": -1} },
         { "Action": {"QuickMenuActions": "VehicleAIToggle"}, "Id": "^", "Number": 0, "InventoryIndex": {"X": -1, "Y": -1} }, 
         { "Action": {"QuickMenuActions": "None"}, "Id": "^", "Number": 0, "InventoryIndex": {"X": -1, "Y": -1} }, 
